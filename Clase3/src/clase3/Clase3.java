6c8634b5ac8249dad1ff811d94e0403967c0dc0b
AshCM <126216673+Ashcm06@users.noreply.github.com>
2025-01-29 20:38:19 -0600

    Add files via upload

---
+++ Clase3/src/clase3/Clase3.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ashca
+ */
+public class Clase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        //* Formas de usar el for*//
+        
+        String resultado = "";
+        for (int i = 0; i < 5; i++) {
+            resultado += "valor i:" + i + "\n";
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+        
+            
+
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
